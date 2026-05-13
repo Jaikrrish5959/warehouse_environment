58af647e2b8aa088d14e81bc7dd5fe0089787cdd
Jaikrrish5959 <jaikrrish5959@gmail.com>
2026-05-13 23:22:22 +0530

    Add .gitignore and remove build artifacts

--- build/warehouse_env/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jaikrrishs/Desktop/warehouse_robot
-# Build directory: /home/jaikrrishs/Desktop/warehouse_robot/build/warehouse_env
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
